fd769095c23b745b2c3c7d33c0832a99b0029273
MicaBelle <84885589+MicaBelle@users.noreply.github.com>
2024-04-05 12:47:47 -0300

    Maven and API basic files

---
+++ src/test/java/HIP/apiTest/ApiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package HIP.apiTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
